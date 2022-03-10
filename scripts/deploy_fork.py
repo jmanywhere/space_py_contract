3d64c7d332f9c565343f1b28c13d77c968908822
jmanywhere <josmfernandez@gmail.com>
2022-03-09 23:31:09 -0600

    ok it works

--- scripts/deploy_fork.py
+++ scripts/deploy_fork.py
@@ -1,12 +1,15 @@
 from brownie import accounts, DustToken
 from scripts.deploy import deploy_libraries
+from web3 import Web3
 
 
 def deploy_token():
     deploy_libraries()
     dust = DustToken.deploy({"from": accounts[0]})
     pair = dust.mainPair()
-    print(f"{pair}")
+    router = dust.uniswapV2Router()
+    print(f"{pair}, {router}")
+    print(f"{Web3.fromWei(accounts[0].balance(),'ether')}")
 
 
 def main():
